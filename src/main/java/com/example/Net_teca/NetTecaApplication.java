5d0bfde7898cf71d3b7173a8a0180c1984ecf29e
Mateus <mateus.ssilva2002@gmail.com>
2026-02-25 10:22:41 -0300

    first commit

---
+++ src/main/java/com/example/Net_teca/NetTecaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Net_teca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetTecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetTecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Net_teca/NetTecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Net_teca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetTecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
